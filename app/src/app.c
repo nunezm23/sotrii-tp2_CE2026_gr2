684fa9f28449f8cda5c31bf33bdaab376212b041
Nekonoir01 <202054074+Nekonoir01@users.noreply.github.com>
2026-08-06 23:17:22 -0500

    Add TP2 activity 03 Active Object Btn

---
+++ app/src/app.c
@@ -0,0 +1,161 @@
+/*
+ * Copyright (c) 2026 Sebastian Bedin <sebabedin@gmail.com> &
+ * 					  Juan Manuel Cruz <jcruz@fi.uba.ar> <jcruz@frba.utn.edu.ar>.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author : Sebastian Bedin <sebabedin@gmail.com> &
+ * 			 Juan Manuel Cruz <jcruz@fi.uba.ar> <jcruz@frba.utn.edu.ar>
+ */
+
+/********************** inclusions *******************************************/
+/* Project includes */
+#include "main.h"
+#include "cmsis_os.h"
+
+/* Demo includes */
+#include "logger.h"
+#include "dwt.h"
+
+/* Application & Tasks includes */
+#include "board.h"
+#include "app_it.h"
+#include "task_a.h"
+#include "task_b.h"
+#include "task_btn_attribute.h"
+#include "task_btn_interface.h"
+#include "task_btn.h"
+#include "task_sys_attribute.h"
+#include "task_sys_interface.h"
+#include "task_sys.h"
+#include "task_led_attribute.h"
+#include "task_led_interface.h"
+#include "task_led.h"
+
+/********************** macros and definitions *******************************/
+#define G_APP_TICK_CNT_INI				0ul
+#define G_TASK_IDLE_CNT_INI				0ul
+#define G_APP_STACK_OVERFLOW_CNT_INI	0ul
+
+/********************** internal data declaration ****************************/
+
+/********************** internal functions declaration ***********************/
+
+/********************** internal data definition *****************************/
+const char *p_app	= "RTOS - Event-Triggered Systems (ETS)";
+const char *p_app_	= "sotrii-tp2_03-application: Demo Code";
+const char *p_app__	= "(Source => CESE - Sistemas Operativos de Tiempo Real)";
+
+/********************** external data declaration ****************************/
+uint32_t volatile g_app_tick_cnt;
+uint32_t g_task_idle_cnt;
+uint32_t g_app_stack_overflow_cnt;
+
+/* Declare a variable of type SemaphoreHandle_t (binary or counting) or mutex.
+ * This is used to reference the semaphore that is used to synchronize a thread
+ * with other thread or to ensure mutual exclusive access to...*/
+
+/* Declare a variable of type TaskHandle_t. This is used to reference threads. */
+TaskHandle_t h_task_a;
+TaskHandle_t h_task_b;
+
+/********************** external functions definition ************************/
+void app_init(void)
+{
+	/*  Declare & Initialize App variables */
+	g_app_tick_cnt = G_APP_TICK_CNT_INI;
+	g_task_idle_cnt = G_TASK_IDLE_CNT_INI;
+	g_app_stack_overflow_cnt = G_APP_STACK_OVERFLOW_CNT_INI;
+
+	/* Enable the Cortex-M4 cycle counter before measuring AO interfaces. */
+	cycle_counter_init();
+
+	/* Print out: Application Initialized */
+	LOGGER_INFO(" ");
+	LOGGER_INFO("%s is running - Tick [mS] = %lu", GET_NAME(app_init), xTaskGetTickCount());
+
+	LOGGER_INFO(" %s is a %s", GET_NAME(app), p_app);
+	LOGGER_INFO(" %s is a %s", GET_NAME(app), p_app_);
+	LOGGER_INFO(" %s is a %s", GET_NAME(app), p_app__);
+
+	/* The semaphore is created in the 'empty' state, meaning the semaphore
+	 * must first be given using the xSemaphoreGive() API function before it can
+	 * subsequently be taken (obtained) using the xSemaphoreTake() function */
+
+	/* Add threads, ... */
+    BaseType_t ret;
+
+    /* Task A thread at priority 2 */
+	ret = xTaskCreate(task_a,							/* Pointer to the function thats implement the task. */
+					  "Task A       ",					/* Text name for the task. This is to facilitate debugging only. */
+					  (configMINIMAL_STACK_SIZE),		/* Stack depth in words. */
+					  NULL,								/* We are not using the task parameter. */
+					  (tskIDLE_PRIORITY + 2ul),			/* This task will run at priority 1. */
+					  &h_task_a);						/* We are using a variable as task handle. */
+
+    /* Check the thread was created successfully. */
+    configASSERT(pdPASS == ret);
+
+    /* Task B thread at priority 2 */
+	ret = xTaskCreate(task_b,							/* Pointer to the function thats implement the task. */
+					  "Task B       ",					/* Text name for the task. This is to facilitate debugging only. */
+					  (configMINIMAL_STACK_SIZE),		/* Stack depth in words. */
+					  NULL,								/* We are not using the task parameter. */
+					  (tskIDLE_PRIORITY + 2ul),			/* This task will run at priority 1. */
+					  &h_task_b);						/* We are using a variable as task handle. */
+
+    /* Check the thread was created successfully. */
+    configASSERT(pdPASS == ret);
+
+    /* Total amount of heap space that remains unallocated. Is also available
+     * with xFreeBytesRemaining variable for heap management schemes 2 to 5.
+     * Memory array used by heap_4 is specified as:
+     * uint8_t ucHeap[configTOTAL_HEAP_SIZE]; */
+    ret = xPortGetFreeHeapSize();
+
+    /* There is no dedicated list for task in Running mode (as we have only
+     * one task in this state at the moment), but the currently run task ID
+     * is stored in variable pxCurrentTCB */
+
+    /* Active Objects Open */
+    open_led_ao(&h_led[LED_A]);
+    open_led_ao(&h_led[LED_B]);
+    open_led_ao(&h_led[LED_C]);
+	open_sys_ao(&h_sys);
+	/* Create one gatekeeper Active Object for each physical button. */
+	open_btn_ao(&h_btn[BTN_A]);
+	open_btn_ao(&h_btn[BTN_B]);
+
+    /* Application Interrupts Init */
+	app_it_init();
+
+}
+
+/********************** end of file ******************************************/
